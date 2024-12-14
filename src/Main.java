7d4d4e8e4dafa1c1703a3b9e23137c22756f57f5
Manuel <caballeromanuel203@gmail.com>
2024-12-14 11:55:07 -0800

    refactored .iml file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import Components.Frame;
+
 import javax.swing.*;
 
-void main(){
-        SwingUtilities.invokeLater(Frame::new);
+class Main {
+        public static void main(String[] args) {
+                SwingUtilities.invokeLater(Frame::new);
+        }
 }
